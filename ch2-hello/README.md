f9d0bc4df9f3b134172b194fbd0616aff3c840c4
venu <venukumarbv@gmail.com>
2024-10-05 12:16:14 +0530

    2 chapters

---
+++ ch2-hello/README.md
@@ -0,0 +1,2 @@
+# DjangoProfessionalPractice
+This is the repository where I practice Django from Django for Professional book
---
+++ ch2-hello/pages/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def home_page_view(request):
+    return HttpResponse("Hello World!")
